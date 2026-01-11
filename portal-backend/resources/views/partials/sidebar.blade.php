c6e3b7a76f55d908678f673d2f883cdf3e68b719
Lycorisme <haldi0230@gmail.com>
2026-01-11 09:20:13 +0700

    feat: remove pages from sidebar menus admin

--- portal-backend/resources/views/partials/sidebar.blade.php
+++ portal-backend/resources/views/partials/sidebar.blade.php
@@ -112,14 +112,7 @@
             <span x-show="sidebarOpen" x-cloak class="font-medium whitespace-nowrap">Galeri</span>
         </a>
 
-        {{-- Kelola Halaman (New) --}}
-        <a href="#"
-            class="flex items-center rounded-xl transition-all duration-200 group {{ request()->routeIs('pages*') ? 'bg-theme-gradient text-white shadow-theme' : 'text-surface-600 dark:text-surface-400 hover:bg-surface-100 dark:hover:bg-surface-800/50' }}"
-            :class="sidebarOpen ? 'gap-3 px-4 py-3' : 'justify-center p-3'"
-            :title="!sidebarOpen ? 'Kelola Halaman' : ''">
-            <i data-lucide="file-text" class="w-5 h-5 flex-shrink-0"></i>
-            <span x-show="sidebarOpen" x-cloak class="font-medium whitespace-nowrap">Kelola Halaman</span>
-        </a>
+
 
         <div class="my-3 border-t border-surface-200/50 dark:border-surface-800/50 mx-2"></div>
 
